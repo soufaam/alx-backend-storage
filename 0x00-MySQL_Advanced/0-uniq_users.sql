bb7489d816c750023a9903ef385aa5f3421e1d06
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-21 22:50:45 +0000

    uniq users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 -- script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
-	`email` VARCHAR(255) NOT NULL,
-	`name` VARCHAR(255) NOT NULL,
-    UNIQUE(`email`)
+	`email` VARCHAR(255) NOT NULL UNIQUE,
+	`name` VARCHAR(255) NOT NULL 
 );
