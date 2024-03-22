7c4d3e2b679e1d1a4d642e9babefb0cf3b5ce472
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-22 00:53:54 +0000

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,8 +2,6 @@
 CREATE TRIGGER decreases
 AFTER INSERT ON `orders`
 FOR EACH ROW
-BEGIN
-    UPDATE `items`
-	SET quantity = quantity - NEW.number
-	WHERE name = NEW.item_name;
-END;
+UPDATE `items`
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
