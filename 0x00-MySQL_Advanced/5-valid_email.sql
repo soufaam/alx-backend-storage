2201381b37d4149050a5f6ab8e5d499c100c1bb4
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-22 01:22:06 +0000

    5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,8 @@
 -- Before TRIGGER
+DELIMITER //
 CREATE TRIGGER resets
-before UPDATE ON `users`
+BEFORE UPDATE ON `users`
 FOR EACH ROW
 	IF NEW.email != OLD.email THEN
 		SET NEW.valid_email = 0 ;
-	END IF;
+	END IF; // DELIMITER;
