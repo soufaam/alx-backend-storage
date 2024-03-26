056e3d418b19c6cc177c825a2682cfac750fb9b1
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-26 01:34:13 +0000

    11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" insert_school """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """update"""
+    return [result for result in mongo_collection.find(
+                                        {'topics': topics})]
