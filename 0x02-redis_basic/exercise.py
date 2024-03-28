353071e04098a95e1fbc8e76fb09a3c6af9c2854
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-28 00:41:16 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
         self._redis = Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union(str, bytes, int, float)) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """return string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
