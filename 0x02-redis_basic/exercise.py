e855ec8aa7c5661e7db9d6ab8c2ac67fdac516aa
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-28 01:58:03 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 from redis import Redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
@@ -17,3 +17,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Union[Callable, type(None)] = None):
+        """A method convert to fn"""
+        if not key:
+            value = self._redis.get(key)
+        elif fn:
+            value = fn(self._redis.get(key))
+        else:
+            value = self._redis.get(key)
+        return value
+
+    def get_str(self, key: str):
+        """get_str"""
+        return self.get(key, str)
+
+    def get_int(self, key):
+        """get_str"""
+        return self.get(key, int)
