42f57ad7996806bb7c9ec135ee42306fd23fae58
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-28 00:28:14 +0000

    exercise.py

---
+++ 0x02-Redis-basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Writing strings to Redis"""
+from redis import Redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """Class Cashe"""
+    def __init__(self):
+        """init method"""
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union(str, bytes, int, float)) -> str:
+        """return string"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
